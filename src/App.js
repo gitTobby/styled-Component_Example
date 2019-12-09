5d5201a6672a891e9288fc84def6c2332c3d21dd
Tobby Kim <itobby@douzone.com>
2019-12-09 14:10:16 +0900

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Greeting from './Greeting/Greeting';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Greeting />
+      </div>
+    );
+  }
+}
+
+export default App;
